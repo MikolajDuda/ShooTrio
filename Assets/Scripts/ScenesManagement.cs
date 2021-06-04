8bc703468e452941d1e8fb84fb1eb80118bffe90
MikolajDuda <duda.mikoaj@gmail.com>
2021-06-04 20:34:31 +0200

    Make basic Menu GUI

---
+++ Assets/Scripts/ScenesManagement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManagement : MonoBehaviour
+{
+    public void StartFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartSecondLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+    public void StartThirdLevel()
+    {
+        SceneManager.LoadScene(3);
+    }
+    
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
